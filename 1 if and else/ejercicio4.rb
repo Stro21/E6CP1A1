47630732d98c5612326bacd55ca73c6a90fe18ad
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-26 20:20:33 -0300

    Se completo el ejercicio 4 if.

--- 1 if and else/ejercicio4.rb
+++ 1 if and else/ejercicio4.rb
@@ -1,7 +1,9 @@
 # Permitir que el usuario pueda ingresar la contraseña por teclado
 # para que se cumpla la condición.
 
-if password == 'secreto'
+puts 'Ingresar contraseña'
+password = gets
+if password.chop == 'secreto'
   puts 'Acceso PERMITIDO! :)'
 else
   puts 'Acceso DENEGADO! :('
