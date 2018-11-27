ab19da679d3651907d04559275466e40f5dd2211
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-27 00:33:46 -0300

    Se empezó con el ejercicio 3 de los ciclos anidados.

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,6 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+#Debido a que no se que se refieren con tabla de multiplicar, supongo que seria
+#las primeras 10 multiplicaciones.
