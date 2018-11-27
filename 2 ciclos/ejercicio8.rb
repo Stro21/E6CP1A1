852c11a0224f544f7b1ea4468363c53c158329ba
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-26 21:16:50 -0300

    Se agrego el ejercicio 8 ciclo.

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -2,8 +2,13 @@
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
 a = ''
-10.times do |i|
-	
+for i in 1..10 do
+	a = a + i.to_s
+	if i % 2 == 0
+		a = a + 'par '
+	else
+		a = a + 'impar '
+	end
 end
 
 puts a
