21af37ae404c6a414d8998faa0abaff51ff041df
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-26 21:42:16 -0300

    Se completo el ejercicio 1 de ciclos anidados.

--- 3 ciclos anidados/ejercicio1.rb
+++ 3 ciclos anidados/ejercicio1.rb
@@ -5,3 +5,15 @@ Se pide imprimir la secuencia numérica, de la siguiente forma:
 3   6   9   12
 4   8   12   16
 =end
+
+matriz = ''
+
+for i in 1..4 do
+  for j in 1..4 do
+    a = i*j
+    matriz = matriz + a.to_s + ' '
+  end
+  matriz = matriz + "\n"
+end
+
+puts matriz
